51847560a17f3b8275c679c69490d603c78e29a4
stanersk <86760552+stanersk@users.noreply.github.com>
2021-08-27 12:58:41 +0900

    Add files via upload

---
+++ vaccine/vaccine/src/main/java/vaccinereservation/ReservationCancelled.java
@@ -0,0 +1,57 @@
+package vaccinereservation;
+
+import java.util.Date;
+
+public class ReservationCancelled extends AbstractEvent {
+
+    private Long id;
+    private Long userId;
+    private String hospital;
+    private Date reservedDate;
+    private String reservationStatus;
+    private Long vaccineId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+    public Date getReservedDate() {
+        return reservedDate;
+    }
+
+    public void setReservedDate(Date reservedDate) {
+        this.reservedDate = reservedDate;
+    }
+    public String getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(String reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
+
+    public Long getVaccineId() {
+        return vaccineId;
+    }
+
+    public void setVaccineId(Long vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+}
